1cb56ea4727e0b9d0696785ee179c6b5918a1717
Danilo Andrade <mandrade.danilo@gmail.com>
2023-04-14 09:09:39 -0300

    French translation
    translation by @invaders-xx

---
+++ resources/lang/fr/clauses.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'true' => 'Est vrai',
+    'false' => 'Est faux',
+    'set' => 'Est défini',
+    'not_set' => 'N\'est pas défini',
+    'equal' => 'Est égal à',
+    'not_equal' => 'N\'est pas égal à',
+    'on_after' => 'Est le ou après le',
+    'on_before' => 'Est le ou avant le',
+    'greater_equal' => 'Est supérieur ou égal à',
+    'less_equal' => 'Est inférieur ou égal à',
+    'greater_than' => 'Est supérieur à',
+    'less_than' => 'Est inférieur à',
+    'between' => 'Est entre',
+    'start_with' => 'Commence par',
+    'not_start_with' => 'Ne commence pas pas',
+    'end_with' => 'Finit par',
+    'not_end_with' => 'Ne finit pas par',
+    'contain' => 'Contient',
+    'not_contain' => 'Ne contient pas',
+
+    'from' => 'De',
+    'until' => 'À',
+    'days' => 'Jours',
+    'weeks' => 'Semaines',
+    'months' => 'Mois',
+    'years' => 'Années',
+    'from_now' => 'À partir de maintenant',
+    'ago' => 'Depuis',
+];
